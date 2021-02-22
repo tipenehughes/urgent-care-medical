22a952ef53e8ddf169ac2050038e8391e718e1b2
Tipene Hughes <tipenehughes@gmail.com>
2021-02-22 14:06:07 -0500

    Added insurance section

--- src/components/insurance/Insurance.js
+++ src/components/insurance/Insurance.js
@@ -2,7 +2,34 @@ import React from "react";
 import styles from "./Insurance.module.css";
 
 const Insurance = () => {
-    return <div></div>;
+    return (
+        <div className={styles.insuranceContainer}>
+            <div className={styles.insuranceBackground}>
+                <div className={styles.insuranceHeader}>
+                    <h3>Insurance</h3>
+                </div>
+                <div className={styles.insuranceBox}>
+                    <div className={styles.flexItem}>
+                        <div className={styles.icon}></div>
+                        <div className={styles.subheader}></div>
+                        <div className={styles.listFlex}>
+                            <div className={styles.list}></div>
+                            <div className={styles.list}></div>
+                        </div>
+                    </div>
+                    <div className={styles.flexItem}>
+                        <div className={styles.icon}></div>
+                        <div className={styles.subheader}></div>
+                        <div className={styles.listFlex}>
+                            <div className={styles.list}></div>
+                            <div className={styles.list}></div>
+                        </div>
+                    </div>
+                    <div className={styles.contact}></div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Insurance;
