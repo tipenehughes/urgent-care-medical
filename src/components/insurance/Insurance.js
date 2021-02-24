7ded42a6902049e325dc9f698dfa4f4c36dee64f
Tipene Hughes <tipenehughes@gmail.com>
2021-02-23 21:04:48 -0500

    Added insurance section content

--- src/components/insurance/Insurance.js
+++ src/components/insurance/Insurance.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiFillCheckCircle } from "react-icons/ai";
 import styles from "./Insurance.module.css";
 
 const Insurance = () => {
@@ -10,22 +11,56 @@ const Insurance = () => {
                 </div>
                 <div className={styles.insuranceBox}>
                     <div className={styles.flexItem}>
-                        <div className={styles.icon}></div>
-                        <div className={styles.subheader}></div>
+                        <div className={styles.icon}>
+                            <AiFillCheckCircle />
+                        </div>
+                        <div className={styles.subheader}>
+                            <h4>We accept most major insurances, including:</h4>
+                        </div>
                         <div className={styles.listFlex}>
-                            <div className={styles.list}></div>
-                            <div className={styles.list}></div>
+                            <div className={styles.list}>
+                                <ul>
+                                    <li>Aetna</li>
+                                    <li>BCBS</li>
+                                    <li>Cigna</li>
+                                    <li>Tricare</li>
+                                </ul>
+                            </div>
+                            <div className={styles.list}>
+                                <ul>
+                                    <li>Ambetter</li>
+                                    <li>Medicare</li>
+                                    <li>United Healtcare</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                     <div className={styles.flexItem}>
-                        <div className={styles.icon}></div>
-                        <div className={styles.subheader}></div>
+                        <div className={styles.icon}>
+                            <AiFillCheckCircle />
+                        </div>
+                        <div className={styles.subheader}>
+                            <h4>We accept most Medicaid plans, including:</h4>
+                        </div>
                         <div className={styles.listFlex}>
-                            <div className={styles.list}></div>
-                            <div className={styles.list}></div>
+                            <div className={styles.list}>
+                                <ul>
+                                    <li>Aetna Better Health</li>
+                                    <li>Amerigroup</li>
+                                    <li>
+                                        Cook Children’s Medicaid/CHIPSuperior
+                                    </li>
+                                    <li>Tricare</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                    <div className={styles.contact}></div>
+                    <div className={styles.contact}>
+                        <p>
+                            Contact us to inquire further about our accepted
+                            insurances
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
